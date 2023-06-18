9be15d8567122f50e12224abe8b96dcd8a18abd0
yul <1103221645@qq.com>
2023-06-18 11:07:09 +0800

    高阶函数

---
+++ javascript/functional/README.md
@@ -0,0 +1,42 @@
+# 函数式编程
+
+## 什么是函数式编程
+
+函数式编程是一个编程范式，对应的还有面向对象编程。
+
+- 面向对象编程：将事物抽象成类和对象，通过封装、继承和多态演示事物间的联系。
+- 函数式编程：将运算过程进行抽象。
+
+```js
+// 面向过程
+let n1 = 2
+let n2 = 3
+let sum = n1 + n2
+console.log(sum)
+
+// 函数式
+function sum(n1, n2) {
+    return n1 + n2
+}
+console.log(sum(1, 2))
+```
+
+# 函数是一等公民
+
+## 函数可以赋值给变量
+
+```js
+// 把函数赋值给变量
+const controller = {
+	index(params) return otherFunc(params)
+}
+// 上面中，index方法的传参和返回跟内部的otherFunc一样，可以这样优化
+const controller = {
+	index: otherFunc // 注意，这里是赋值这个函数，不需要加括号
+}
+```
+
+## 高阶函数
+
+- 可以把函数作为参数传递给另外一个函数
+- 可以把函数作为另一个函数的返回结果
---
+++ javascript/functional/higher-order-function.js
@@ -0,0 +1,39 @@
+// 高阶函数-函数作为参数
+
+function forEach(arr, fn) {
+  for(let i = 0; i < arr.length; i++) {
+    fn(arr[i])
+  }
+}
+
+let arr = [1,2,3,4]
+forEach(arr, (item) => {
+  console.log(item)
+})
+
+
+function filter(arr, fn) {
+  const res = []
+  for(let i = 0; i < arr.length; i++) {
+    if(fn(arr[i])) res.push(arr[i])
+  }
+  return res
+}
+
+const testFilterArr = filter(arr, (item) => {return item > 2})
+console.log(testFilterArr)
+
+
+// 高级函数-函数作为返回值
+
+function makeFn() {
+  let msg = "function1"
+  return function() {
+    console.log(msg)
+  }
+}
+
+makeFn()()
+
+// 只执行一个：once
+
